5fcac2d688e106a09a099e4c7cfba7b6ec244fc2
Liviu Cauia <liviu.cauia@gmail.com>
2020-02-26 11:09:36 +0200

    ok

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -2,5 +2,4 @@ Rails.configuration.stripe = {
     :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
     :secret_key => ENV['STRIPE_SECRET_KEY']
 }
-
-Stripe.api_key = 'sk_test_fnWAiUU648A6Kj7zZy214b5w00QeU8esSe'
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
